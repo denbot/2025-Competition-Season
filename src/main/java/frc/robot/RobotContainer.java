75bcdc1cdbca4c1a0e28016a848cade9142e2efa
Reuben Kaskubar <rrkask@gmail.com>
2025-01-15 17:47:30 -0700

    Changed to lower case v for "vision" in folder, GoToReefCommand, and RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -30,7 +30,7 @@ import frc.robot.subsystems.drive.GyroIOPigeon2;
 import frc.robot.subsystems.drive.ModuleIO;
 import frc.robot.subsystems.drive.ModuleIOSim;
 import frc.robot.subsystems.drive.ModuleIOTalonFX;
-import frc.robot.Vision.commands.GoToReefCommand;
+import frc.robot.vision.commands.GoToReefCommand;
 
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 
--- src/main/java/frc/robot/Vision/commands/GoToReefCommand.java
+++ src/main/java/frc/robot/Vision/commands/GoToReefCommand.java
@@ -2,7 +2,7 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.Vision.commands;
+package frc.robot.vision.commands;
 
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
@@ -10,7 +10,7 @@ import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Robot;
-import frc.robot.Vision.LimelightHelpers;
+import frc.robot.vision.LimelightHelpers;
 
 /* You should consider using the more terse Command factories API instead https://docs.wpilib.org/en/stable/docs/software/commandbased/organizing-command-based.html#defining-commands */
 public class GoToReefCommand extends Command {
