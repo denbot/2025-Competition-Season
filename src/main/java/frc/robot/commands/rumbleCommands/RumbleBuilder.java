b926a257a2c629e721a576d677627d15f8795367
Justin Nesselrotte <admin@jnesselr.org>
2025-03-18 12:41:29 -0600

    Added Javadoc explaining how the RumbleBuilder DSL works

--- src/main/java/frc/robot/commands/rumbleCommands/RumbleBuilder.java
+++ src/main/java/frc/robot/commands/rumbleCommands/RumbleBuilder.java
@@ -3,6 +3,21 @@ package frc.robot.commands.rumbleCommands;
 import edu.wpi.first.wpilibj.GenericHID;
 import frc.robot.subsystems.RumbleSubsystem;
 
+/**
+ * This class represents a DSL (Domain Specific Language) to generate a set of rumble commands. The DSL forces a few
+ * constraints:
+ * 1. You must start with a rumble.
+ * 2. You can pulse twice in a row, but can't pause twice in a row.
+ * 3. You can't end with a pause.
+ * <p>
+ * We enforce these constraints by treating specific classes as nodes in a state machine. RumbleBuilder only has a pulse
+ * method to start a rumble, satisfying the first condition.
+ * <p>
+ * It returns a MainBuilder. The MainBuilder actually has the build method, so we can build after a rumble now. It has a
+ * wait method that returns the PulseBuilder. The PulseBuilder can only pulse and returns MainBuilder again. It cannot
+ * build itself or wait, and therefore enforces the second and third conditions. The MainBuilder inherits from the
+ * PulseBuilder only so we don't have to write the same pulse method twice.
+ */
 public class RumbleBuilder implements CanAddPulse {
   private final RumbleSubsystem rumbleSubsystem;
 
@@ -47,7 +62,6 @@ public class RumbleBuilder implements CanAddPulse {
     }
   }
 
-  // Whenever we see this class, we last had a pulse
   public static class MainBuilder extends PulseBuilder {
     MainBuilder(PulseBuilder existing, Pulse nextPulse) {
       super(existing, nextPulse);
