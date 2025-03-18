356c653d9ec6d70bb25f3e865ffb462b57cb0024
Justin Nesselrotte <admin@jnesselr.org>
2025-03-18 11:30:30 -0600

    ReefTarget now combines direction and april tag

--- src/main/java/frc/robot/game/ReefAprilTag.java
+++ src/main/java/frc/robot/game/ReefAprilTag.java
@@ -1,18 +1,20 @@
 package frc.robot.game;
 
 public enum ReefAprilTag {
-  TWO(9, 22),
-  FOUR(8, 17),
-  SIX(7, 18),
-  EIGHT(6, 19),
-  TEN(11, 20),
-  TWELVE(10, 21);
+  TWO(9, 22, 120),
+  FOUR(8, 17, 60),
+  SIX(7, 18, 0),
+  EIGHT(6, 19, -60),
+  TEN(11, 20, -129),
+  TWELVE(10, 21, 180);
 
   public final double red;
   public final double blue;
+  public final double angle; // -180 to 180, from the driver station perspective
 
-  ReefAprilTag(double red, double blue) {
+  ReefAprilTag(double red, double blue, double angle) {
     this.red = red;
     this.blue = blue;
+    this.angle = angle;
   }
 }
