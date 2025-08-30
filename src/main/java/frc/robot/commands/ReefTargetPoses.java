1944d3ff6d8a82b0e30e51d16d5e06b6e4d6a2fb
Stefan <BlyatBoio5@gmail.com>
2025-08-29 23:34:52 -0600

    RT Added More Reef Setpoints for Red Team

--- src/main/java/frc/robot/commands/ReefTargetPoses.java
+++ src/main/java/frc/robot/commands/ReefTargetPoses.java
@@ -3,18 +3,18 @@ package frc.robot.commands;
 import frc.robot.game.ReefAprilTag;
 
 public enum ReefTargetPoses {
-  TWO_LEFT(6.0, 2.8, 120, ReefAprilTag.TWO, Direction.LEFT),
-  FOUR_LEFT(3.75, 2.8, 60, ReefAprilTag.FOUR, Direction.LEFT),
-  SIX_LEFT(2.8, 3.5, 0, ReefAprilTag.SIX, Direction.LEFT),
-  EIGHT_LEFT(3.75, 4.25, -60, ReefAprilTag.EIGHT, Direction.LEFT),
-  TEN_LEFT(5.85, 3.75, -120, ReefAprilTag.TEN, Direction.LEFT),
-  TWELVE_LEFT(6.55, 4.25, 180, ReefAprilTag.TWELVE, Direction.LEFT),
-  TWO_RIGHT(6.5, 2.1, 120, ReefAprilTag.TWO, Direction.RIGHT),
-  FOUR_RIGHT(3.25, -2.5, 60, ReefAprilTag.FOUR, Direction.RIGHT),
-  SIX_RIGHT(2.8, 4.5, 0, ReefAprilTag.SIX, Direction.RIGHT),
-  EIGHT_RIGHT(3.25, 4.55, -60, ReefAprilTag.EIGHT, Direction.RIGHT),
-  TEN_RIGHT(5.55, 4.55, -120, ReefAprilTag.TEN, Direction.RIGHT),
-  TWELVE_RIGHT(6.55, 3.75, 180, ReefAprilTag.TWELVE, Direction.RIGHT);
+  TWO_LEFT(5.0, 2.8, 120, ReefAprilTag.TWO, Direction.LEFT),
+  FOUR_LEFT(3.7, 3, 60, ReefAprilTag.FOUR, Direction.LEFT),
+  SIX_LEFT(3.2, 4.2, 0, ReefAprilTag.SIX, Direction.LEFT),
+  EIGHT_LEFT(3.7, 5.0, -60, ReefAprilTag.EIGHT, Direction.LEFT),
+  TEN_LEFT(5.0, 5.2, -120, ReefAprilTag.TEN, Direction.LEFT),
+  TWELVE_LEFT(5.7, 3.8, 180, ReefAprilTag.TWELVE, Direction.LEFT),
+  TWO_RIGHT(5.2, 3.0, 120, ReefAprilTag.TWO, Direction.RIGHT),
+  FOUR_RIGHT(3.9, 2.8, 60, ReefAprilTag.FOUR, Direction.RIGHT),
+  SIX_RIGHT(3.2, 3.8, 0, ReefAprilTag.SIX, Direction.RIGHT),
+  EIGHT_RIGHT(4.0, 5.2, -60, ReefAprilTag.EIGHT, Direction.RIGHT),
+  TEN_RIGHT(5.2, 5.0, -120, ReefAprilTag.TEN, Direction.RIGHT),
+  TWELVE_RIGHT(5.7, 4.1, 180, ReefAprilTag.TWELVE, Direction.RIGHT);
 
   // OTF variables
   public double x;
