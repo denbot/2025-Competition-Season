d280a0a8bbbf6e4c5897626263e4ae32e15220f5
Stefan <BlyatBoio5@gmail.com>
2025-08-29 18:29:41 -0600

    OTF defined pathfinder command

--- src/main/java/frc/robot/commands/OnTheFlyAlignCommand.java
+++ src/main/java/frc/robot/commands/OnTheFlyAlignCommand.java
@@ -3,14 +3,13 @@ package frc.robot.commands;
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.path.PathConstraints;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.drive.Drive;
 
 public class OnTheFlyAlignCommand extends Command {
-  private static PathConstraints constraints =
-      new PathConstraints(3.0, 3.0, Units.degreesToRadians(540), Units.degreesToRadians(720));
-
+  // private static PathConstraints constraints;
   Command pathFindingCommand;
   public Pose2d targetPose;
 
@@ -21,9 +20,17 @@ public class OnTheFlyAlignCommand extends Command {
   }
 
   @Override
+  public void initialize() {}
+
+  @Override
   public void execute() {
     System.out.println("Executing On-The-Fly Command");
-    pathFindingCommand = AutoBuilder.pathfindToPose(this.targetPose, constraints);
+
+    pathFindingCommand =
+        AutoBuilder.pathfindToPose(
+            new Pose2d(0, 0, new Rotation2d(0)),
+            new PathConstraints(
+                3.0, 3.0, Units.degreesToRadians(540), Units.degreesToRadians(720)));
   }
 
   @Override
