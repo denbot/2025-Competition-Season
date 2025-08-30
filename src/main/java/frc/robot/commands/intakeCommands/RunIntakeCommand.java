90046d2fc9cbd332434831ecdf146a4a5be0972b
Stefan <BlyatBoio5@gmail.com>
2025-08-29 23:51:00 -0600

    BR Finished Reverting BH Refactor

--- src/main/java/frc/robot/commands/intakeCommands/RunIntakeCommand.java
+++ src/main/java/frc/robot/commands/intakeCommands/RunIntakeCommand.java
@@ -3,10 +3,10 @@ package frc.robot.commands.intakeCommands;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.Constants.IntakeConstants;
 import frc.robot.Robot;
+import frc.robot.commands.boathookCommands.HandoffCommand;
 import frc.robot.subsystems.boathook.Boathook;
 import frc.robot.subsystems.boathook.Boathook.Level;
 import frc.robot.subsystems.intake.Intake;
@@ -29,13 +29,14 @@ public class RunIntakeCommand extends Command {
   private final Timer runningWait = new Timer();
 
   private final IntakeMoveCommand liftToL1;
-  private SequentialCommandGroup runHandoff;
+  private final HandoffCommand runHandoff;
 
   public RunIntakeCommand(Intake intake, Boathook boathook, Direction direction) {
     this.intake = intake;
     this.boathook = boathook;
     this.direction = direction;
     this.liftToL1 = new IntakeMoveCommand(intake, false, IntakeConstants.intakeL1Angle, 1, 2);
+    this.runHandoff = new HandoffCommand(boathook, intake);
     addRequirements(this.intake);
   }
 
