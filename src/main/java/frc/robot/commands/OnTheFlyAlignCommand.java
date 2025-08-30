f54e94f294d909834dce67dc85d19d3a37c23a03
Stefan <BlyatBoio5@gmail.com>
2025-08-30 00:11:28 -0600

    OTF Moved red side inversion to OTF Command class

--- src/main/java/frc/robot/commands/OnTheFlyAlignCommand.java
+++ src/main/java/frc/robot/commands/OnTheFlyAlignCommand.java
@@ -5,6 +5,7 @@ import com.pathplanner.lib.path.PathConstraints;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.RobotContainer;
 import frc.robot.subsystems.drive.Drive;
@@ -22,20 +23,26 @@ public class OnTheFlyAlignCommand extends Command {
 
   @Override
   public void execute() {
-    System.out.println(
-        "Aligning To: "
-            + RobotContainer.currentTargetPose.x
-            + ", "
-            + RobotContainer.currentTargetPose.y);
+
+    double x = RobotContainer.currentTargetPose.x;
+    double y = RobotContainer.currentTargetPose.y;
+    double angle = RobotContainer.currentTargetPose.angle;
+    // if the alliance is red, flip positions accordingly
+    if (DriverStation.getAlliance().isPresent()
+        && DriverStation.getAlliance().get() == DriverStation.Alliance.Red) {
+      // approximate location of top right corner of the reef = 17.6, 7.6
+      x = 17.6 - x;
+      y = 8.05 - y;
+      angle += 180;
+      if (angle > 180) angle -= 360;
+    }
+    System.out.println("Aligning To: " + x + ", " + y);
 
     // initializes new pathFindToPose command which both create a path and has the robot follow said
     // path
     pathFindingCommand =
         AutoBuilder.pathfindToPose(
-            new Pose2d(
-                RobotContainer.currentTargetPose.x,
-                RobotContainer.currentTargetPose.y,
-                new Rotation2d(Units.degreesToRadians(RobotContainer.currentTargetPose.angle))),
+            new Pose2d(x, y, new Rotation2d(Units.degreesToRadians(angle))),
             new PathConstraints(
                 2.0, 2.0, Units.degreesToRadians(540), Units.degreesToRadians(720)));
   }
