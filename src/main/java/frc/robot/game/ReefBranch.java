4ebb3627d9c58714345dcbc88440691ef09f7fbe
Antonio C Delgado <91296497+acdelgado1019@users.noreply.github.com>
2026-01-05 19:03:11 -0700

    Converted Two Left to AprilTagLayout format, need to verify Red/Blue flip

--- src/main/java/frc/robot/game/ReefBranch.java
+++ src/main/java/frc/robot/game/ReefBranch.java
@@ -1,16 +1,27 @@
 package frc.robot.game;
 
 public enum ReefBranch {
-  TWO_LEFT,
-  TWO_RIGHT,
-  FOUR_LEFT,
-  FOUR_RIGHT,
-  SIX_LEFT,
-  SIX_RIGHT,
-  EIGHT_LEFT,
-  EIGHT_RIGHT,
-  TEN_LEFT,
-  TEN_RIGHT,
-  TWELVE_LEFT,
-  TWELVE_RIGHT
+  TWO_LEFT(9, 22, ReefBranchOffset.LEFT),
+  TWO_RIGHT(9, 22, ReefBranchOffset.RIGHT),
+  FOUR_LEFT(8, 17, ReefBranchOffset.LEFT),
+  FOUR_RIGHT(8, 17, ReefBranchOffset.RIGHT),
+  SIX_LEFT(7, 18, ReefBranchOffset.LEFT),
+  SIX_RIGHT(7, 18, ReefBranchOffset.RIGHT),
+  EIGHT_LEFT(6, 19, ReefBranchOffset.LEFT),
+  EIGHT_RIGHT(6, 19, ReefBranchOffset.RIGHT),
+  TEN_LEFT(11, 20, ReefBranchOffset.LEFT),
+  TEN_RIGHT(11, 20, ReefBranchOffset.RIGHT),
+  TWELVE_LEFT(10, 21, ReefBranchOffset.LEFT),
+  TWELVE_RIGHT(10, 21, ReefBranchOffset.RIGHT);
+
+  public final int redTag;
+  public final int blueTag;
+  public final ReefBranchOffset offset;
+
+  ReefBranch(int redTag, int blueTag, ReefBranchOffset offset) {
+    this.redTag = redTag;
+    this.blueTag = blueTag;
+    this.offset = offset;
+  }
 }
+
